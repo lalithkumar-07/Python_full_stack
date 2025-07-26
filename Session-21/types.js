d4192be264fada7254489eae3ccfc02d09cd567a
Lalith Kumar Goud Nanam <nanamlalithkumar@gmail.com>
2025-07-26 10:45:22 +0530

    c16

---
+++ Session-21/types.js
@@ -0,0 +1,13 @@
+let a = true;
+let b = false;
+console.log(a, b);
+
+const colors = ['red', 'green', 'blue'];
+
+const data = new Date(2023, 10, 1); 
+const person = {
+    fstname: 'John',
+    lstname: 'Doe'
+};
+let text ="bot:" + person.fstname + "<br> ";
+text += "bot:" + person['fstname'] ;
