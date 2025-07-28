484d1a7310710273e22c3f60789efbd73a92fcd7
Lalith Kumar Goud Nanam <nanamlalithkumar@gmail.com>
2025-07-28 14:45:12 +0530

    c1

---
+++ Session-24/script.js
@@ -0,0 +1,54 @@
+// // let arr=["apple", "banana", "cherry"];
+// // console.log(arr);
+
+// // arr.push("orange");
+// // console.log(arr); 
+// // arr.pop();
+// // console.log(arr[0]);
+// // arr[1] = "blueberry";
+// // console.log(arr);
+// let person = {
+//     name: "Sandeep",
+//     age: 21,
+//     city: "Medak"
+// }
+// function d(x,y,z){
+//     return (x+y+z);
+// }
+// console.log(d(45,7,18));
+// // let tri = ()=>{
+// //     return 0.5 * 3 * 4;
+// // }
+// function check(Score){
+//     if(Score>=100){
+//         return true;
+//     }
+//     else{
+//         return false;
+//     }
+// }
+// console.log(check(120));
+// let p=prompt();
+// if(p=="easy"){
+//     console.log("You have selected easy level");
+// }
+// else if(p=="medium"){
+//     console.log("You have selected medium level");
+// }
+// else if(p=="hard"){
+//     console.log("You have selected hard level");
+// }
+// else{
+//     console.log("Please select a valid level");
+// }
+
+// function hello(){
+//     console.log("Hello, World!");
+// }
+// hello();
+// function greet(name){
+//     console.log("Hello, " + name + "! How are you?");
+// }
+
+// greet("Sandeep");
+
